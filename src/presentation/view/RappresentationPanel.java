852c4a1d0edbd29cae2ee95a332ca4ab25b59c88
Nicola Gabriele <nicolagabriele99@gmail.com>
2022-05-18 14:57:37 +0200

    gui

--- src/presentation/view/RappresentationPanel.java
+++ src/presentation/view/RappresentationPanel.java
@@ -1,8 +1,24 @@
 package presentation.view;
 
+import logical_unit.organizzation_charts.OrganizzationChart;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class RappresentationPanel extends JPanel {
 
+
+    private OrganizzationChart root;
+
+    public RappresentationPanel(OrganizzationChart root) {
+        this.root = root;
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        new SimpleChartRappresentation(root).paintComponent(g2d);
+    }
 
 }//Rappresentation
